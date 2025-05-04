3af084005e8159965eaa477fb507b7e5c295174e
Username-not-used <lasteward@icloud.com>
2025-05-04 17:13:23 -0500

    Update Process.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Process.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Process.java
@@ -9,6 +9,33 @@ package org.firstinspires.ftc.teamcode;
  * @author Asher Childress - 9161 Overload
  */
 public class Process {
-
+    private String name;
+    private ProcessStatus status;
+    private Timer timer;
+    
+   /**
+     * Constructor for a basic process.
+     * 
+     * @param name The name of the process
+     */
+    public BasicProcess(String name) {
+        this.name = name;
+        this.status = ProcessStatus.NOT_STARTED;
+        this.statusChecker = null;
+        this.timer = new Timer();
+    }
+    public void setStatus(ProcessStatus status) {
+        this.status = status;
+    }
+  
+    public void startProcess(String processName) {
+      TimedProcess process = processes.get(processName);
+      if (process != null) {
+          process.start();
+      }
+    }
+    public TimedProcess removeProcess(String processName) {
+        return processes.remove(processName);
+    }
 
 }
