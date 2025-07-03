14e88e38edb37627d6f215b91d7ffd09bdfc9c48
Username-not-used <lasteward@icloud.com>
2025-07-03 16:48:11 -0500

    Update ArmSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSubsystem.java
@@ -1,1 +1,18 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArmSubsystem extends Subsystem {
+    private ArrayList<PDFLController> PDFLController;
+    
+    public ArmSubsystem(ArrayList<PDFLController> PDFLController, ArrayList<HardwareDevice> devices, ArrayList<DcMotorEx> motors, ArrayList<Servo> servos, double[] position) {
+      super(devices, motors, servos, position);
+      this.PDFLController = PDFLController;
+      
+    }
+}
