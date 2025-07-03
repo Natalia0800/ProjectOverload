4640c91000e6cd919236675c8150f9281e1b5fca
Username-not-used <lasteward@icloud.com>
2025-07-03 17:33:39 -0500

    Update ArmSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSubsystem.java
@@ -8,11 +8,20 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ArmSubsystem extends Subsystem {
-    private ArrayList<PDFLController> PDFLController;
+    private PDFLController Controller1;
+    
     
     public ArmSubsystem(ArrayList<PDFLController> PDFLController, ArrayList<HardwareDevice> devices, ArrayList<DcMotorEx> motors, ArrayList<Servo> servos, double[] position) {
       super(devices, motors, servos, position);
       this.PDFLController = PDFLController;
       
+    }
+
+
+    public void setPosition(int pos) {
+        Controller1.setTarget(pos);
+    }
+    public void armUpdate(int pos) {
+        Controller1.update(pos);
     }
 }
